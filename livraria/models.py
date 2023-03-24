f62aff849a39d3cfe2fd6357ab5b1ee0a50681d9
Marco André Mendes <marcoandre@gmail.com>
2023-03-24 14:32:31 -0300

    criação da model Autor

--- livraria/models.py
+++ livraria/models.py
@@ -1,16 +1,29 @@
 from django.db import models
 
+
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.descricao} ({self.id})"
 
+
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
-    site = models.URLField(null=True, blank=True) # esse campo é opcional
+    site = models.URLField(null=True, blank=True)  # esse campo é opcional
 
     def __str__(self):
         return self.nome
 
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=255)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self):
+        return self.nome
+    
+    class Meta:
+        verbose_name_plural = "Autores"
+    
 
