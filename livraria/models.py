d247f3017907bf0fb0493a869a413fc6438459b9
Marco André Mendes <marcoandre@gmail.com>
2023-03-24 15:02:26 -0300

    incluida a chave estrangeira Categoria no Livro

--- livraria/models.py
+++ livraria/models.py
@@ -5,7 +5,7 @@ class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.descricao} ({self.id})"
+        return self.descricao
 
 
 class Editora(models.Model):
@@ -32,6 +32,9 @@ class Livro(models.Model):
     isbn = models.CharField(max_length=32, null=True, blank=True)
     quantidade = models.IntegerField(default=0, null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros"
+    )
 
     def __str__(self):
         return f"{self.titulo} ({self.quantidade})"
